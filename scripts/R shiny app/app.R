48aae808603fe34a4827da9bd408d825d5b04646
MaudBV <maudbv@gmail.com>
2022-10-08 21:34:35 +0200

    add theme and sidebar text

--- scripts/R shiny app/app.R
+++ scripts/R shiny app/app.R
@@ -12,6 +12,7 @@ library(sysfonts)
 library(dplyr)
 require(shinyWidgets)
 require(shinyjs)
+require(shinythemes)
 
 # import and pre-process data
 source('data processing.R')
@@ -27,46 +28,60 @@ source("resources/functions/plot_chrono.R")
 
 #Set up User Interface
 ui <- bootstrapPage(
+  navbarPage(theme = shinytheme("flatly"),
+             collapsible = TRUE,
+             HTML('<a style="text-decoration:none;cursor:default;color:#FFFFFF;" class="active" href="#">Hypotheses in Invasion Biology</a>'),
+             id="nav",
+             windowTitle = "Exploring hypotheses in Invasion Biology",
+             
+             tabPanel("Evidence for each hypothesis",
+                      sidebarLayout(
+                        sidebarPanel(
+                          
+                          "Explore the evidence available for major hypotheses in Invasion Science.
+                          You can filter studies by taxa,
+                          habitats or research method",
+                          
+                          selectInput('hyp', 'Select a hypothesis',
+                                      c(unique(total_df$hypothesis))),
+                          
+                          pickerInput(inputId = 'taxa',
+                                      'Select a taxonomic group',
+                                      choices = taxa_groups,
+                                      multiple = TRUE,
+                                      selected = taxa_groups,
+                                      options = list(`actions-box` = TRUE,
+                                                     `none-selected-text` = "Please make a selection!")
+                          ),
+                          
+                          uiOutput("habitat_selector"),
+                          
+                          uiOutput("method_selector"),
+                          
   
-  titlePanel('Exploring hypotheses in Invasion Biology'),
-  
-  sidebarLayout(
-    sidebarPanel(
-      
-      "Select the hypothesis you wish to explore.
-      You will then be able to filter by taxa, habitats and research method",
-      
-      selectInput('hyp', 'Select a hypothesis',
-                  c(unique(total_df$hypothesis))),
-    
-      pickerInput(inputId = 'taxa',
-                  'Select a taxonomic group',
-                  choices = taxa_groups,
-                  multiple = TRUE,
-                  selected = taxa_groups,
-                  options = list(`actions-box` = TRUE,
-                     `none-selected-text` = "Please make a selection!")
-      ),
-      
-      uiOutput("habitat_selector"),
-      
-      uiOutput("method_selector")
-    ),
-    
-    #### TODO
-    # FIX the taxa name typos
-    # make taxa_group reactive depending on the hypothesis
-    # improve formatting as a dashboard/fluidpage ?
-    # switch to plotly interactive
-    # add donut plot for summary stats
-    # add radar plot for subhyps
-    
-  mainPanel(
-    tabsetPanel(
-      # tabPanel("Support for the hypothesis", plotlyOutput('chronology')) 
-      tabPanel("Support for the hypothesis", plotOutput('chronology')) 
-  )
-  )
+                          HTML('<a style="text-decoration:none;cursor:default;color:#27596B;" class="active" href="#">This is a, project of the Hi Knowledge initiative</a>'),
+                          tags$br(),
+                          tags$a(href="hi-knowledge.org", "hi-knowledge.org")
+                        )
+                      ,
+                      
+                      #### TODO
+                      # FIX the taxa name typos
+                      # make taxa_group reactive depending on the hypothesis
+                      # improve formatting as a dashboard/fluidpage ?
+                      # switch to plotly interactive
+                      # add donut plot for summary stats
+                      # add radar plot for subhyps
+                      
+                      mainPanel(
+                        tabsetPanel(
+                          # tabPanel("Support for the hypothesis", plotlyOutput('chronology')) 
+                          tabPanel("Support over time",
+                                   plotOutput('chronology')) 
+                        )
+                      )
+                      )
+             )
   )
 )
 
